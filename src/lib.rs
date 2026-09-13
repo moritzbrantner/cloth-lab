f8a491a6b034081c6b465d8befb3f9f47c84e755
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-13 17:41:48 +0200

    Export cloth collider API

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod cloth;
 mod math;
 
 pub use cloth::{
-    Cloth, ClothError, DistanceConstraint, FixedStepConfig, Particle, RectangularClothConfig,
-    StepReport,
+    Cloth, ClothCollider, ClothError, DistanceConstraint, FixedStepConfig, Particle,
+    RectangularClothConfig, SphereCollider, StepReport,
 };
 pub use math::Vec3;
